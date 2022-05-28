ec5519368177cde692028be2732dd39b6399d045
Patryk7077 <pchwalczyk@student.agh.edu.pl>
2022-05-28 23:12:05 +0200

    Added levels of difficulty

--- main.py
+++ main.py
@@ -25,25 +25,46 @@ class Logic():
     def __init__(self):
         self.formatted_base = []
         self.current_word = None
+        self.number_of_words = None
+        self.number_of_guessed = 0
+        self.number_of_mistakes = 0
         self.words_base = pd.read_excel(db)
-        for index, row in self.words_base.iterrows():
-            self.formatted_base.append(Word(row['english'], row['polish'], row['difficulty']))
+    def newBase(self, diff=None):
+        if diff == None:
+            for index, row in self.words_base.iterrows():
+                self.formatted_base.append(Word(row['english'], row['polish'], row['difficulty']))
+        else:
+            for index, row in self.words_base.iterrows():
+                if row['difficulty'] == diff:
+                    self.formatted_base.append(Word(row['english'], row['polish'], row['difficulty']))
+        self.number_of_words = len(self.formatted_base)
+    def clearAnswers(self):
+        self.formatted_base = []
+        self.current_word = 0
+        self.number_of_guessed = 0
+        self.number_of_mistakes = 0
     def randNewWord(self):
         if len(self.formatted_base) == 0:
-            return None
+            return False
         self.current_word = random.randint(0,len(self.formatted_base)-1)
-        return self.formatted_base[self.current_word].english
+        return True
     def checkAnswer(self, input):
         if(self.formatted_base[self.current_word].polish != input):
-            self.current_word = None
+            self.current_word = 0
             return False
         else:
             self.formatted_base.pop(self.current_word)
-            self.current_word = None
+            self.current_word = 0
+            self.number_of_guessed += 1
             return True
+    def getCurrentWord(self):
+        if self.current_word != None:
+            return self.formatted_base[self.current_word].english
+        else:
+            return "ERROR"
+    def getCounter(self):
+        return str(self.number_of_guessed)+"/"+str(self.number_of_words)
 
-logic = Logic()
-text = logic.randNewWord()
 
 
 class SampleApp(ctk.CTk):
@@ -79,7 +100,11 @@ class SampleApp(ctk.CTk):
         '''Show a frame for the given page name'''
         frame = self.frames[page_name]
         frame.tkraise()
-
+    def update_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.update()
+    def get_frame(self, page_name):
+        return self.frames[page_name]
 
 class StartPage(ctk.CTkFrame):
 
@@ -105,47 +130,89 @@ class StartPage(ctk.CTkFrame):
         label.pack(side="top", fill="x", pady=10)
 
         button1 = ctk.CTkButton(self, text="Do fiszek",
-                            command=lambda: controller.show_frame("PageOne"))
+                            command=lambda: chooseDifficulty())
         button1.pack()
 
         button2 = ctk.CTkButton(self, text="Dodaj słowo",
                             command=lambda: controller.show_frame("AddWordPage"))
         button2.pack()
 
-class PageOne(ctk.CTkFrame):
+        button3 = ctk.CTkButton(self, text="Graj na wybranym poziomie trudności",
+                                command=lambda: createToplevel())
+        button3.pack()
 
+        def createToplevel():
+            window = ctk.CTkToplevel(self)
+            window.geometry("400x200")
+            window.title("Wybierz poziom trudności")
+
+            button_easy = ctk.CTkButton(window,text="Łatwy",command=lambda: closeToplevel("łatwy"))
+            button_easy.place(x=10,y=150)
+            button_medium = ctk.CTkButton(window,text="Średni",command=lambda: closeToplevel("średni"))
+            button_medium.place(x=135, y=150)
+            button_hard = ctk.CTkButton(window,text="Trudny",command=lambda: closeToplevel("trudne"))
+            button_hard.place(x=260, y=150)
+
+            def closeToplevel(diff):
+                chooseDifficulty(diff)
+                window.destroy()
+
+        def chooseDifficulty(diff=None):
+            logic.newBase(diff)
+            logic.randNewWord()
+            app.frames["PageOne"].T1.config(state='normal')
+            app.frames["PageOne"].T1.delete("0", "end")
+            app.frames["PageOne"].T1.insert(ctk.END, logic.getCurrentWord())
+            app.frames["PageOne"].T1.config(state='disable')
+            app.frames["PageOne"].counter.config(text=logic.getCounter())
+            app.show_frame("PageOne")
+
+logic = Logic()
+
+class PageOne(ctk.CTkFrame):
     def __init__(self, parent, controller):
         ctk.CTkFrame.__init__(self, parent)
         self.controller = controller
         button = ctk.CTkButton(self, text="Powrót do głównego menu",
-                           command=lambda: controller.show_frame("StartPage"))
+                           command=lambda: close_window())
         button.pack()
 
-        T1 = ctk.CTkEntry(self, height=1, width=200)
-        T1.insert(ctk.END, text)
-        T1.config(state='disable')
-        T1.place(x=50,y=378)
+        self.T1 = ctk.CTkEntry(self, height=1, width=200)
+        self.T1.insert(ctk.END, str(logic.getCurrentWord()))
+        self.T1.config(state='disable')
+        self.T1.place(x=50, y=378)
 
         T2 = ctk.CTkEntry(self, height=1, width=200)
         T2.place(x=800, y=378)
 
-        button2 = ctk.CTkButton(self, text="Sprawdź odpowiedź", width=25, command=lambda: action(T1, T2))
+        button2 = ctk.CTkButton(self, text="Sprawdź odpowiedź", width=25, command=lambda: checkAnswer(self.T1,self.counter))
         button2.place(x=500, y=450)
 
+        self.counter = ctk.CTkLabel(self,text=logic.getCounter())
+        self.counter.place(x=300,y=100)
 
-        def action(t1, t2):
+        def updateEntries(T1,counter):
+            T1.config(state='normal')
+            T1.delete("0", "end")
+            T1.insert(ctk.END, logic.getCurrentWord())
+            T1.config(state='disable')
+            T2.delete("0", "end")
+            T2.insert(ctk.END, '')
+            counter.config(text=logic.getCounter())
+
+        def checkAnswer(T1,counter):
             ans = logic.checkAnswer(T2.get())
-            text = logic.randNewWord()
+            logic.randNewWord()
             if ans:
                 mb.showwarning("Odpowiedź", "To poprawna odpowiedź!")
             else:
                 mb.showerror("Odpowiedź", "Niestety, jest to niepoprawna odpowiedź!")
-            T1.config(state='normal')
-            T1.delete("0","end")
-            T1.insert(ctk.END, text)
-            T1.config(state='disable')
-            T2.delete("0","end")
-            T2.insert(ctk.END,'')
+            updateEntries(T1,counter)
+
+        def close_window():
+            controller.show_frame("StartPage")
+            self.update()
+            logic.clearAnswers()
 
 class AddWordPage(ctk.CTkFrame):
 
@@ -177,9 +244,9 @@ class AddWordPage(ctk.CTkFrame):
         add_word.place(x=400, y=460)
 
         def add_word(pl,en,diff):
-            if pl.get('1.0','end-1c') and en.get('1.0','end-1c') and diff.get():
+            if pl.get() and en.get() and diff.get():
                 try:
-                    new_word = pd.DataFrame([{'polish':pl.get('1.0','end-1c'), 'english':en.get('1.0','end-1c'), 'difficulty':diff.get()}])
+                    new_word = pd.DataFrame([{'polish':pl.get(), 'english':en.get(), 'difficulty':diff.get()}])
                     wb = load_workbook(filename = "baza_slowek_polsko_angielskie.xlsx")
                     ws = wb["Arkusz1"]
                     for r in dataframe_to_rows(new_word, index=False, header=False):
@@ -192,7 +259,7 @@ class AddWordPage(ctk.CTkFrame):
                     en.insert(ctk.END,"")
                     diff.set('')
                 except:
-                    mb.showerror("Informacja", "Błąd")
+                    mb.showerror("Informacja", pl.get())
             else:
                 mb.showerror("Informacja", "Błąd w wprowadzaniu danych!")
 
